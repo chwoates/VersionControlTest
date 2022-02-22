2ade4486d97c01b32017f7166004a07c380a0575
oates <chwoates@gmail.com>
2022-02-21 17:40:35 -0700

    FIrst code

--- src/VersionControl/VersionCont.java
+++ src/VersionControl/VersionCont.java
@@ -1,2 +1,15 @@
-package VersionControl;public class VersionCont {
+package VersionControl;
+
+public class VersionCont {
+
+    public static void main(String[] args){
+        VersionCont test = new VersionCont();
+        test.progRun();
+        System.out.println("Dog");
+    }
+
+    public void progRun(){
+        System.out.println("Cat");
+    }
+
 }
